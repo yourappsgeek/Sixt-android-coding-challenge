eb4400358cd1ef42a2ba7f5c3f7a26b3caacb676
yourappsgeek <info.aliuetian@gmail.com>
2020-05-15 10:29:49 +0500

    Refactor CarViewModelTest class

--- app/src/test/java/com/sixt/codingtask/CarViewModelTest.kt
+++ app/src/test/java/com/sixt/codingtask/CarViewModelTest.kt
@@ -23,7 +23,7 @@ import org.mockito.*
  *         Author Email: info.aliuetian@gmail.com
  *         Created on: 2020-02-17
  */
-class MVVMKoinUnitTest : KoinTest {
+class CarViewModelTest : KoinTest {
 
     @Mock
     private lateinit var repository: CarDataSource
@@ -36,10 +36,14 @@ class MVVMKoinUnitTest : KoinTest {
 
     private val carViewModel: CarViewModel by inject()
 
+    @Mock
     private lateinit var isViewLoadingObserver: Observer<Boolean>
-    private lateinit var onMessageErrorObserver: Observer<Any>
+
+    @Mock
     private lateinit var emptyListObserver: Observer<Boolean>
-    private lateinit var onRenderMuseumsObserver: Observer<List<Car>>
+
+    @Mock
+    private lateinit var carsObserver: Observer<List<Car>>
 
     private lateinit var carEmptyList: List<Car>
     private lateinit var carList: List<Car>
@@ -63,20 +67,22 @@ class MVVMKoinUnitTest : KoinTest {
         MockitoAnnotations.initMocks(this)
         Mockito.`when`(context.applicationContext).thenReturn(context)
 
+        carViewModel.apply {
+
+            isLoading.observeForever(isViewLoadingObserver)
+            isEmpty.observeForever(emptyListObserver)
+            cars.observeForever(carsObserver)
+        }
+
         mockData()
-        setupObservers()
     }
 
     @Test
     fun `empty cars list with Repository and ViewModel`() {
-        with(carViewModel) {
-            isLoading.observeForever(isViewLoadingObserver)
-            isEmpty.observeForever(emptyListObserver)
-            cars.observeForever(onRenderMuseumsObserver)
-        }
 
         Mockito.verify(repository, Mockito.times(1)).retrieveCars(capture(operationCallbackCaptor))
         operationCallbackCaptor.value.onSuccess(carEmptyList)
+
         Assert.assertNotNull(carViewModel.isLoading.value)
         Assert.assertTrue(carViewModel.isEmpty.value == true)
         Assert.assertTrue(carViewModel.cars.value?.size == 0)
@@ -84,10 +90,6 @@ class MVVMKoinUnitTest : KoinTest {
 
     @Test
     fun `successful cars list with Repository and ViewModel`() {
-        with(carViewModel) {
-            isLoading.observeForever(isViewLoadingObserver)
-            cars.observeForever(onRenderMuseumsObserver)
-        }
 
         Mockito.verify(repository, Mockito.times(1)).retrieveCars(capture(operationCallbackCaptor))
         operationCallbackCaptor.value.onSuccess(carList)
@@ -98,28 +100,18 @@ class MVVMKoinUnitTest : KoinTest {
 
     @Test
     fun `failure cars list with Repository and ViewModel`() {
-        with(carViewModel) {
-            isLoading.observeForever(isViewLoadingObserver)
-            onError.observeForever(onMessageErrorObserver)
-        }
+
         Mockito.verify(repository, Mockito.times(1)).retrieveCars(capture(operationCallbackCaptor))
         operationCallbackCaptor.value.onError("Something went wrong. please try again later!")
+
         Assert.assertNotNull(carViewModel.isLoading.value)
         Assert.assertNotNull(carViewModel.onError.value)
     }
 
-    private fun setupObservers() {
-        isViewLoadingObserver = Mockito.mock(Observer::class.java) as Observer<Boolean>
-        onMessageErrorObserver = Mockito.mock(Observer::class.java) as Observer<Any>
-        emptyListObserver = Mockito.mock(Observer::class.java) as Observer<Boolean>
-        onRenderMuseumsObserver = Mockito.mock(Observer::class.java) as Observer<List<Car>>
-    }
-
     private fun mockData() {
         carEmptyList = emptyList()
-        carList = mutableListOf<Car>().apply {
+        carList = listOf(
 
-            add(
                 Car(
                     "1",
                     "Vicki+",
@@ -130,10 +122,8 @@ class MVVMKoinUnitTest : KoinTest {
                     11.563875,
                     "VERY_CLEAN",
                     ""
-                )
-            )
+                ),
 
-            add(
                 Car(
                     "2",
                     "Casimir",
@@ -144,10 +134,8 @@ class MVVMKoinUnitTest : KoinTest {
                     11.563875,
                     "VERY_CLEAN",
                     ""
-                )
-            )
+                ),
 
-            add(
                 Car(
                     "3",
                     "Quentin",
@@ -160,7 +148,6 @@ class MVVMKoinUnitTest : KoinTest {
                     ""
                 )
             )
-        }.toList()
     }
 
     @After
